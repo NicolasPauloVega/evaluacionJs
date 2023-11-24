0db5ec36bf5951bdf2a4fde9df3ad6eda602cbd3
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2023-11-24 18:24:03 -0500

    A1

--- js/seguros.js
+++ js/seguros.js
@@ -1,36 +1,33 @@
+//Evaluacion de javaScript.
+
+//funcion que le permita al usuario comprar un servicio
 function mostrar(){
 
-    const seguros = document.getElementById('seguros');
+    opc = parseInt(prompt("Ingresa el numero perteneciente al servicio:\n1.Servicio de vida.\n2.Servicio de salud.\n3.Servicio de moto.\n4.Servicio de accidentes.\n5.Ninguna de los anteriores servicios."))
 
-    const cantidad = document.getElementById('cantidad');
-
-    let calcular = 0;
-    let valor = 0;
-
-    switch(seguros){
-
+    switch(opc){
         case 1:
-            valor = 100;
-            calcular = cantidad * valor;
+            cantidad = parseInt(prompt("Ingresa la cantidad del servicio que deseas comprar: "))
+            total = 100 * cantidad;
+            document.getElementById('total').innerHTML = "El total de la compra es: $"+total;
             break;
         case 2:
-            valor = 200;
-            calcular = cantidad * valor;
+            cantidad = parseInt(prompt("Ingresa la cantidad del servicio que deseas comprar: "))
+            total = 150 * cantidad;
+            document.getElementById('total').innerHTML = "El total de la compra es: $"+total;
             break;
         case 3:
-            valor = 300;
-            calcular = cantidad * valor;
+            cantidad = parseInt(prompt("Ingresa la cantidad del servicio que deseas comprar: "))
+            total = 200 * cantidad;
+            document.getElementById('total').innerHTML = "El total de la compra es: $"+total;
             break;
         case 4:
-            valor = 400;
-            calcular = cantidad * valor;
+            cantidad = parseInt(prompt("Ingresa la cantidad del servicio que deseas comprar: "))
+            total = 180 * cantidad;
+            document.getElementById('total').innerHTML = "El total de la compra es: $"+total;
             break;
         default:
-            document.getElementById('total').innerHTML = "No se realizo la compra del seguro";
-            return;
+            document.getElementById('total').innerHTML = "No se escogio ninguno de nuestros servicios.";
+            break;
     }
-
-    var totalS = calcular;
-
-    document.getElementById('total').innerHTML = totalS
 }
